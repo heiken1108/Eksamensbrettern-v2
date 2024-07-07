ba8e0a9e0dd78904caf8ca25eb21c21868278840
Håkon Hoelsæter <hakon1108@gmail.com>
2024-07-07 21:53:22 +0200

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/legg-til-oppgave">Gå til side for legge til oppgave</Link>
+      <Link href="/legg-til-oppgave">Gå til side for legge til oppgaver</Link>
     </main>
   );
 }
